42d5e095fbe8a2f87c1d3cd87ad74f1011461c12
Mehdi Benkhraba <elmehdi.benkhraba@gmail.com>
2023-06-15 16:20:01 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 /**
  * main -  prints exactly "and that piece of art is useful"
  * - Dora Korpar, 2015-10-19",
- * Return: Always 1
+ * Return: Always 0
  */
 int main(void)
 {
 	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(2, str1, 59);
-	return (1);
+	return (0);
