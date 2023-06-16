1fb45e8229c93e35bdf4d2ee1859a5588577e6b2
Mehdi Benkhraba <elmehdi.benkhraba@gmail.com>
2023-06-16 15:04:50 -0700

    print alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*
+ * main - Prints the alphabet in lowercase except q and e
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char alphabet[24] = "abcdfghijklmnoprstuvwxyz";
+	int i;
+
+	for (i = 0; i < 24; i++)
+	{
+		putchar(alphabet[i]);
+	}
+	putchar('\n');
+	return (0);
+}
