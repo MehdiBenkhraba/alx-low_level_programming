11d9a6c6cc9961e3ca43eead1993bc52cd034a4e
Mehdi Benkhraba <elmehdi.benkhraba@gmail.com>
2023-06-20 15:07:49 -0700

    integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of integer
+ * @i: i is  integer
+ *
+ * Return: integer
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
